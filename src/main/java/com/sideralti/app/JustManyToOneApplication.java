58a4162344340c6b235b55ecd8fee2409ca2d33b
Sideral TI <sideralti.app@gmail.com>
2024-12-08 00:59:49 -0300

    commit inicial d JustManyToOne

---
+++ src/main/java/com/sideralti/app/JustManyToOneApplication.java
@@ -0,0 +1,13 @@
+package com.sideralti.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustManyToOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustManyToOneApplication.class, args);
+	}
+
+}
